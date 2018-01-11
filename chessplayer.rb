14bb0c26836b95fb1df8bb8240ee1d37f974a2ab
Jacob Louie <jlouie9@gmail.com>
2018-01-10 21:58:16 -0500

    Convert input # into two numbers array

--- chessplayer.rb
+++ chessplayer.rb
@@ -8,7 +8,7 @@ class Player
     @disc = disc
   end
 
-  def take_turn(options, stdin = $stdin)
+  def take_turn_from(options, stdin = $stdin)
     print "\nPlease select the chess piece you want to move by selecting the square it currently occupies(column# first, then row#).\n> "
     input1 = stdin.gets.chomp.to_i
 
@@ -16,7 +16,10 @@ class Player
       print "That is an incorrect value! Try again:\n> "
       input1 = stdin.gets.chomp.to_i
     end
+    convert_input(input1)
+  end
 
+  def take_turn_to(options, stdin = $stdin)
     print "\nPlease select the square where you want to move your chess piece to(column# first, then row#).\n> "
     input2 = stdin.gets.chomp.to_i
 
@@ -24,7 +27,15 @@ class Player
       print "That is an incorrect value! Try again:\n> "
       input2 = stdin.gets.chomp.to_i
     end
+    convert_input(input2)
+  end
 
-    input2
+  def convert_input(input)
+    if input.to_s.length == 1
+      input = input.to_s.rjust(2, "0").split("")
+    else
+      input = input.to_s.split("")
+    end
+    input
   end
 end
